46698f66571cfe02eeb9d503951b8919d8e7d056
shimulkhan <khanshimul196@gmail.com>
2021-10-15 03:07:40 +0600

    added login-form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo2.png';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h2>this is login</h2>
+        <div className="d-flex justify-content-center align-items-center">
+            <div className="w-25">
+                <div className="mt-5">
+                    <div className="text-center"><img src={logo} width="220" alt="" /></div>
+                    <Form className="mt-4 login-form w-100">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="Email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <button className="w-100 py-2 btn-login mb-2" type="submit">
+                            Sign In
+                        </button>
+                    </Form>
+                    <div className="text-center"><Link to="/signup" className="already-exist primary text-center">Already have an account?</Link></div>
+                </div>
+            </div>
         </div>
     );
 };
