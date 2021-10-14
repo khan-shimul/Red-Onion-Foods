3688833055d11979554d3c724d06c3231c75237c
shimulkhan <khanshimul196@gmail.com>
2021-10-14 15:45:44 +0600

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-link">
+      <h1 className="fw-bold">kire madari</h1>
+      <Button variant="danger">Button</Button>
     </div>
   );
 }
